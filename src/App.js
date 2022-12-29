2202f2604762929975f420657c3bf2f47af5c8e7
banusoppan <kjksopan@gmail.com>
2022-12-29 20:05:06 +0530

    Added cart component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sigin-in-and-sign-up/sign-in-and-sign-up.component'
-import { Route ,Switch} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-// const Hats = () =>{
-//   return(
-//     <div>
-//     <h1>HATS</h1>
-//   </div>
-//   )
- 
-// }
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
-function App() {
+const App = () => {
   return (
-    <div >
-      {/* <HomePage/> */}
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/shop" component={ShopPage}></Route>
-        <Route exact path="/signin" component={SignInAndSignUp}></Route>
-
-      </Switch>
-      
-     
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
